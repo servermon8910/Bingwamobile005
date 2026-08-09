6b8964bf01998762b4b1597b24283e360e20cab0
servermon8910 <servermon8910@gmail.com>
2026-08-09 20:34:38 +0000

    chore: bump version to 3.5.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.bingwa.mobile"
         minSdk = 21
         targetSdk = 35
-        versionCode = 454
-        versionName = "3.5.25"
+        versionCode = 455
+        versionName = "3.5.26"
     }
 
     signingConfigs {
